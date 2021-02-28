daeb44bbb467f6c2dc306d28982ee4cbfe17a299
juuheek <72034446+juuheek@users.noreply.github.com>
2021-02-27 17:39:20 -0800

    Add files via upload

---
+++ comps/Downarrow/index.js
@@ -0,0 +1,34 @@
+function DownArrowUI(){
+  return `<i class="arrow down" style='
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    padding: 1px; 
+    margin-right:10px;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    display:flex;
+    flex-direction:column;
+    width:7px;
+    height:7px;
+  '>
+ 
+  </i>
+  <i class="arrow down" style='
+    border: solid black;
+    border-width: 0 3px 3px 0;
+    
+    padding: 1px; 
+    margin-right:10px;
+    margin-bottom:10px;
+    transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    display:flex;
+    flex-direction:column;
+    width:7px;
+    height:7px;
+  '>
+ 
+  </i>`
+}
+
+// export const DownArrow = DownArrowUI();
---
+++ comps/No/index.js
@@ -0,0 +1,17 @@
+function NoButtonUI(){
+  return `<div style='
+  background-color:#FFF;
+  border: black solid 1px;
+  border-radius:15px 15px;
+  min-height:54px;
+  min-width:144px;
+  display:inline-flex;
+  justify-content:center;
+  align-items:center;
+  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+  '>
+  No
+  </div>`
+}
+
+// export const NoButton = NoButtonUI();
