8e9b9ab8244796320e9692ca56c6fd93aba86924
GlianBCIT <gpalma1@my.bcit.ca>
2021-03-07 14:26:06 -0800

    text

--- comps/WhiteBG/index.js
+++ comps/WhiteBG/index.js
@@ -1,11 +1,13 @@
-function whiteUI(){
+function whiteUI(text=" - Thinness Culture in media <br> - Professionals and careers that promote being thin <br> - Family and childhood traumas <br> - Peer pressure"){
   return `<div style='
     background-color: #FFFFFF;
     width: 350px;
     height: 226px;
     box-shadow: inset 0px 4px 4px rgba(0,0,0,0.25);
     border-radius: 2%;
+    color:#8A8A8A;
   '>
+  ${text}
   </div>
   `
 }
