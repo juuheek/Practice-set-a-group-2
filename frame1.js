1be239b610d06e75910bb1f77ef073844045bfc4
GlianBCIT <gpalma1@my.bcit.ca>
2021-03-07 19:16:16 -0800

    Frame 3

---
+++ frame1.js
@@ -0,0 +1,2 @@
+
+document.querySelector(".learnmore").innerHTML +=pinkButton("Start", "frame2.html");
---
+++ frame3.js
@@ -0,0 +1,4 @@
+document.querySelector('#button1').innerHTML += pinkButton()
+document.querySelector('#button2').innerHTML += pinkButton()
+document.querySelector('#button3').innerHTML += pinkButton()
+document.querySelector("#backarrow").innerHTML += BackArrowUI()
