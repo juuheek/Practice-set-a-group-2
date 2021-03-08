ffc95d7cdf4206608625b7b42ac300f49b5efd5a
juuheek <jkim844@my.bcit.ca>
2021-03-07 19:36:20 -0800

    frame 3

--- .history/frame3_20210307193126.js
+++ .history/frame3_20210307193126.js
@@ -1,4 +1,4 @@
 document.querySelector('#button1').innerHTML += pinkButton("COVID-19","frame4.html")
-document.querySelector('#button2').innerHTML += pinkButton("EATING DISORDER","frame5.html")
+document.querySelector('#button2').innerHTML += pinkButton("EATING DISORDER","frame")
 document.querySelector('#button3').innerHTML += pinkButton("DEPRESSION","frame4.html")
 document.querySelector("#backarrow").innerHTML += BackArrowUI()
