8aa633e5e236a9c5e99807ce09dd6805541f8508
juuheek <jkim844@my.bcit.ca>
2021-03-07 20:06:13 -0800

    changed frame 4

--- .history/frame4_20210307200444.js
+++ .history/frame4_20210307200444.js
@@ -1,2 +1,2 @@
 document.querySelector('#button1').innerHTML += pinkButton("CONTINUE", "frame6.html")
-document.querySelector("#backarrow").innerHTML += BackArrowUI("frame3.html")
+document.querySelector("#backarrow").innerHTML += BackArrowUI()
