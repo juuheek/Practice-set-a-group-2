0cdec45c4b37773d03b8f154a393e16f24c862cb
juuheek <jkim844@my.bcit.ca>
2021-03-08 17:20:29 -0800

    changed heading and links

--- .history/frame15_20210308171834.js
+++ .history/frame15_20210308171834.js
@@ -2,5 +2,5 @@ document.querySelector("#heading").innerHTML += HeaderText("#26325B", "Have you 
 document.querySelector("#whitebg").innerHTML += whiteUI(" • Regularly eat too much food <br> •  Sometimes re-chewed  <br> and swallowed or spit out <br> •May eat quickly or more food <br>than intended  <br> • Feelings of guilt, shame", "1.6")
 document.querySelector("#yesbutton").innerHTML += NoButtonUI("Yes")
 document.querySelector("#nobutton").innerHTML += NoButtonUI()
-document.querySelector("#backarrow").innerHTML += BackArrowUI("Frame12.html")
+document.querySelector("#backarrow").innerHTML += BackArrowUI("Frame16.html")
 document.querySelector('#smile').innerHTML += smileItem()
